0ad686ef9ed529d9a2b70a6692b3fb75a32c2914
mid <midiman@ya.ru>
2025-03-12 21:50:16 +0100

    add simple difficulty factor to terrain generator

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -7,9 +7,13 @@ public class TerrainGenerator : MonoBehaviour
 
     public int width = 512;
     public int height = 512;
-    public int maxHeight = 6;
+    public int maxHeight = 20;
     public float scale = 50f;
     public float tileSize = 10;
+
+    public float flatRadius = 20f;
+    public float difficultyCoef = 2;
+    public float difficultyCoefLinear = 2;
 
     void Start()
     {
@@ -28,17 +32,36 @@ public class TerrainGenerator : MonoBehaviour
     float[,] GenerateHeights()
     {
         float[,] heights = new float[width, height];
+        Vector2 center = new Vector2(width / 2f, height / 2f);
+        float maxDistance = Vector2.Distance(Vector2.zero, center);
+
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                float xCoord = (float)x / width * scale;
-                float yCoord = (float)y / height * scale;
-                heights[x, y] = Mathf.PerlinNoise(xCoord, yCoord);
+                Vector2 point = new Vector2(x, y);
+                float distance = Vector2.Distance(point, center);
+
+                if (distance < flatRadius)
+                {
+                    heights[x, y] = 0f; // Плоская зона
+                }
+                else
+                {
+                    float normalizedDistance = distance / maxDistance;
+                    // float distanceFactor = Mathf.Pow(normalizedDistance, difficultyExponent);
+                    float distanceFactor = normalizedDistance * difficultyCoef;
+                    float xCoord = (float)x / width * scale;
+                    float yCoord = (float)y / height * scale;
+                    float noise = Mathf.PerlinNoise(xCoord, yCoord);
+                    heights[x, y] = noise * distanceFactor + normalizedDistance * difficultyCoefLinear;
+                }
             }
         }
+
         return heights;
     }
+
 
     void AddTexture(TerrainData terrainData)
     {
@@ -47,5 +70,5 @@ public class TerrainGenerator : MonoBehaviour
         layer.tileSize = new Vector2(tileSize, tileSize); // Размер тайла текстуры, можно регулировать
 
         terrainData.terrainLayers = new TerrainLayer[] { layer }; // Добавляем слой
-}
+    }
 }
